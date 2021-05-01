f97ce8ac49754aea3402f68cb500654acf88e4d7
amphetkid <phin.pope@gmail.com>
2021-05-01 08:09:55 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh -l
 
 composer install --no-ansi --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
-
-cp .env.example .env
+ mkdir storage
+        cd storage
+        mkdir logs
+        mkdir framework
+        mkdir framework/cache
+        mkdir framework/cache/data
+        mkdir framework/sessions
+        mkdir framework/testing
+        mkdir framework/views
+        chgrp -R www-data ../storage
+        chown -R www-data ../storage
+        cd ..
+cp .env.ci .env
 
 php artisan key:generate
 
