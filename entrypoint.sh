4bd2ce95559a03d0ab47af889f56202770291071
Phin <79580089+phinpope@users.noreply.github.com>
2021-08-03 12:32:53 +0300

    Update entrypoint.sh
    secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -l
 
 composer install --no-ansi --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
- mkdir storage
-        cd storage
-        mkdir logs
-        mkdir framework
-        mkdir framework/cache
-        mkdir framework/cache/data
-        mkdir framework/sessions
-        mkdir framework/testing
-        mkdir framework/views
-        chgrp -R www-data ../storage
-        chown -R www-data ../storage
-        cd ..
-        echo $STAGING_ENV_FILE > .env
+mkdir storage
+cd storage
+mkdir logs
+mkdir framework
+#mkdir framework/cache
+#mkdir framework/cache/data
+#mkdir framework/sessions
+#kdir framework/testing
+#kdir framework/views
+chgrp -R www-data ../storage
+chown -R www-data ../storage
+cd ..
+echo ${{ secrets.STAGING_ENV_FILE }} > .env
 #cp .env.ci .env
 touch database/reporting.sqlite
 php artisan key:generate
